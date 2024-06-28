0981c96c73bf08d1417c09ced3c04fe07d5cd886
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-28 13:36:01 +0100

    "Added max-lg:border-r-[1px] to three div elements in Tokenomics component"

--- src/Components/Tokenomics.tsx
+++ src/Components/Tokenomics.tsx
@@ -8,19 +8,19 @@ const Tokenomics = () => {
                   
                   <div className="flex flex-row max-lg:flex-col items-center justify-center">
   
-                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-goldText">
+                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] max-lg:border-r-[1px] border-goldText">
                           <div className="flex flex-col space-y-5 h-full items-center justify-center">
                               <p className="text-[16px] font-inter tracking-[0.2rem] text-white">COMMUNITY FUND</p>
                               <p className="text-goldText font-inter text-[40px]">10%</p>
                           </div>
                       </div>
-                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-goldText">
+                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] max-lg:border-r-[1px] border-goldText">
                           <div className="flex flex-col space-y-5 h-full items-center justify-center">
                               <p className="text-[16px] font-inter tracking-[0.2rem] text-white">LIQUIDITY POOLS</p>
                               <p className="text-goldText font-inter text-[40px]">40%</p>
                           </div>
                       </div>
-                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-r-[1px] border-goldText">
+                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-r-[1px] s border-goldText">
                           <div className="flex flex-col space-y-5 h-full items-center justify-center">
                               <p className="text-[16px] font-inter tracking-[0.2rem] text-white">PRESALE</p>
                               <p className="text-goldText font-inter text-[40px]">50%</p>
@@ -31,13 +31,13 @@ const Tokenomics = () => {
   
                   <div className="flex flex-row max-lg:flex-col items-center justify-center">
   
-                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-b-[1px] border-goldText">
+                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] max-lg:border-r-[1px] border-b-[1px] border-goldText">
                           <div className="flex flex-col space-y-5 h-full items-center justify-center">
                               <p className="text-[16px] font-inter tracking-[0.2rem] text-white">TOTAL SUPPLY</p>
                               <p className="text-goldText font-inter text-[20px] ">600000009</p>
                           </div>
                       </div>
-                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-b-[1px] border-goldText">
+                      <div className="h-[260px] max-lg:w-[337px] max-lg:h-[136.17px] w-[383.33px] border-l-[1px] border-t-[1px] border-b-[1px] max-lg:border-r-[1px] border-goldText">
                           <div className="flex flex-col space-y-5 h-full items-center justify-center">
                               <p className="text-[16px] font-inter tracking-[0.2rem] text-white">TOKEN NAME</p>
                               <p className="text-goldText font-inter text-[20px] ">JAPANESE CAT</p>
