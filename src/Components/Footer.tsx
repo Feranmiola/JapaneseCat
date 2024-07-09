a657b1163f2a4a66338a35fffa337ae67aac4164
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-09 15:44:36 +0100

    "Fix typo in footer text: 'OPPIRTUNITES' -> 'OPPORTUNITES'"

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => (
                     <div className="flex flex-col items-center justify-between">
                         <div className="border-b-[1px] border-tokenomicsBg w-full items-center justify-center flex flex-col py-10">
                             <p className="text-[#BB1605] text-[24px] font-semibold">FOLLOW THE LUCKY CAT</p>
-                            <p className="text-[16px] tracking-[0.2rem]">STAY CONNECTED WITH ENDLESS FORTUNE AND OPPIRTUNITES</p>
+                            <p className="text-[16px] tracking-[0.2rem]">STAY CONNECTED WITH ENDLESS FORTUNE AND OPPORTUNITES</p>
                         </div>
                         <div className="flex flex-row h-full">
                             <div className="w-[582.5px] flex items-center py-8 border-r-[1px] border-tokenomicsBg flex-row space-x-5 justify-center">
