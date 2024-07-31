f7296b5d86641ba7743688a45dca9c4b76e9f0c4
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-31 14:25:39 +0100

    "Refactored Roadmap component with updated layout, styling, and added SVG element"

--- src/Components/Roadmap.tsx
+++ src/Components/Roadmap.tsx
@@ -6,51 +6,119 @@ const Roadmap = () => {
             <div className=" relative">
                 <div className="flex  flex-col space-y-20 items-center justify-center">
                     <p className="font-electroharmonix text-[48px] text-headerText max-sm:text-[32px] 3xl:text-[200px] max-lg:test-[32px]">ROADMAP</p>
-                    <div className="flex flex-col  items-center justify-center space-y-5">
+                    <div className="flex relative flex-row items-center justify-center">
+                        <div className="flex flex-col space-y-[3rem]">
 
-                        <div className="w-[859px] max-lg:w-[340px] max-lg:h-[176px] h-[184px] border-[1px] border-[#BB1605]">
-                            <div className="flex flex-row max-sm:flex-col max-sm:space-y-5 lg:space-x-20 py-10 max-sm:py-5 lg:items-center h-full max-sm:justify-center max-sm:pl-[3rem]  lg:pl-10">
-                                <p className="text-[#BB1605] max-sm:font-medium max-sm:text-[20px] font-bold text-[28px]">PHASE 1</p>
+                            <div className="flex flex-row justify-center ">
+                                <div className="flex flex-col w-[370px] h-[184px] border-[2px] border-[#BB1605]">
+                                    <div className="w-full h-1/2 items-center flex justify-center border-b-[2px] border-[#BB1605]">
+                                        <p className="text-[20px] text-[#BB1605] font-medium tracking-[0.6rem] text-cente">PHASE 1</p>
+                                    </div>
+                                    <div className="w-full h-1/2 items-center flex justify-center">
+                                        <p className="text-[20px] text-[#BB1605] font-bold text-center">CONCEPT & PLANNING</p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center w-[488px]">
+                                    <div className="flex flex-col w-full h-[184px] border-[2px] border-l-0 border-[#BB1605]">
+                                        <ul className=" mt-10 h-full pl-16">
+                                            <li className="text-[20px] text-[#BB1605]">IDEA DEVELOPMENT</li>
+                                            <li className="text-[20px] text-[#BB1605]">MARKET RESEARCH</li>
+                                            <li className="text-[20px] text-[#BB1605]">TECHNICAL PLANNING</li>
+                                        </ul>
+                                    </div>
 
-                                <ul>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">PRESALE MARKETING</li>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">PRESALE ON PURPLESALE</li>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">LAUNCH</li>
-                                </ul>
+                                    <div className="bg-[#BB1605] w-full items-center flex justify-center h-[22px]">
+                                        <p className="text-white text-[12px] tracking-[0.4rem] font-bold text-center">COMPLETED</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className="flex flex-row justify-center ">
+                                <div className="flex flex-col w-[370px] h-[184px] border-[2px] border-[#BB1605]">
+                                    <div className="w-full h-1/2 items-center flex justify-center border-b-[2px] border-[#BB1605]">
+                                        <p className="text-[20px] text-[#BB1605] font-medium tracking-[0.6rem] text-cente">PHASE 2</p>
+                                    </div>
+                                    <div className="w-full h-1/2 items-center flex justify-center">
+                                        <p className="text-[20px] text-[#BB1605] font-bold text-center">DESIGN & DEVELOPMENT</p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center w-[488px]">
+                                    <div className="flex flex-col w-full h-[184px] border-[2px] border-l-0 border-[#BB1605]">
+                                        <ul className=" mt-10 h-full pl-16">
+                                            <li className="text-[20px] text-[#BB1605]">UI/UX DESIGNING</li>
+                                            <li className="text-[20px] text-[#BB1605]">APP DEVELOPMENT</li>
+                                            <li className="text-[20px] text-[#BB1605]">BETA LAUNCH</li>
+                                        </ul>
+                                    </div>
+                                    <div className="bg-[#DFB850] w-full items-center flex justify-center h-[22px]">
+                                        <p className="text-[#BB1605] text-[12px] tracking-[0.4rem] font-bold text-center">IN PROGRESS</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            <div className="flex flex-row justify-center ">
+                                <div className="flex flex-col w-[370px] h-[184px] border-[2px] border-[#BB1605]">
+                                    <div className="w-full h-1/2 items-center flex justify-center border-b-[2px] border-[#BB1605]">
+                                        <p className="text-[20px] text-[#BB1605] font-medium tracking-[0.6rem] text-cente">PHASE 3</p>
+                                    </div>
+                                    <div className="w-full h-1/2 items-center flex justify-center">
+                                        <p className="text-[20px] text-[#BB1605] font-bold text-center">OFFICIAL LAUNCH & TGE</p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center w-[488px]">
+                                    <div className="flex flex-col w-full h-[184px] border-[2px] border-l-0 border-[#BB1605]">
+                                        <ul className=" mt-10 h-full pl-16">
+                                            <li className="text-[20px] text-[#BB1605]">PUBLIC LAUNCH</li>
+                                            <li className="text-[20px] text-[#BB1605]">TGE (TOKEN GENERATION EVENT)</li>
+                                            <li className="text-[20px] text-[#BB1605]">TOKEN DISTRIBUTION</li>
+                                        </ul>
+                                    </div>
+                                    <div className="bg-transparent w-full items-center flex justify-center h-[22px]">
+                                        <p className="text-transparent text-[12px] tracking-[0.4rem] font-bold text-center">COMPLETED</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            <div className="flex flex-row justify-center ">
+                                <div className="flex flex-col w-[370px] h-[184px] border-[2px] border-[#BB1605]">
+                                    <div className="w-full h-1/2 items-center flex justify-center border-b-[2px] border-[#BB1605]">
+                                        <p className="text-[20px] text-[#BB1605] font-medium tracking-[0.6rem] text-cente">PHASE 4</p>
+                                    </div>
+                                    <div className="w-full h-1/2 items-center flex justify-center">
+                                        <p className="text-[20px] text-[#BB1605] font-bold text-center">LONG TERM VISION</p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center w-[488px]">
+                                    <div className="flex flex-col w-full h-[184px] border-[2px] border-l-0 border-[#BB1605]">
+                                        <ul className=" mt-10 h-full pl-16">
+                                            <li className="text-[20px] text-[#BB1605]">ECOSYSTEM INTEGRATION</li>
+                                            <li className="text-[20px] text-[#BB1605]">SUSTAINABLE GROWTH</li>
+                                            <li className="text-[20px] text-[#BB1605]">FUTURE DEVELOPMENTS</li>
+                                        </ul>
+                                    </div>
+                                    <div className="bg-transparent w-full items-center flex justify-center h-[22px]">
+                                        <p className="text-transparent text-[12px] tracking-[0.4rem] font-bold text-center">COMPLETED</p>
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
-
-                        <div className="w-[859px] max-lg:w-[340px] max-lg:h-[176px] h-[184px] border-[1px] border-[#BB1605]">
-                            <div className="flex flex-row max-sm:flex-col max-sm:space-y-5 lg:space-x-20 py-10 max-sm:py-5 lg:items-center h-full max-sm:justify-center max-sm:pl-[3rem]  lg:pl-10">
-                                <p className="text-[#BB1605] max-sm:font-medium max-sm:text-[20px] font-bold text-[28px]">PHASE 2</p>
-
-                                <ul>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">POST LAUNCH MARKETING</li>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">AVEDEX, DEXTOOLS, DEXSCREENER TRENDING</li>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">CMC, CG LISTINGS</li>
-                                </ul>
-
-                            </div>
+                        <div className="absolute top-[-25px] left-[22.7rem]">
+                            <svg width="12" height="1020" viewBox="0 0 12 1020" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.666667 6C0.666667 8.94552 3.05448 11.3333 6 11.3333C8.94552 11.3333 11.3333 8.94552 11.3333 6C11.3333 3.05448 8.94552 0.666667 6 0.666667C3.05448 0.666667 0.666667 3.05448 0.666667 6ZM0.666623 1014C0.666622 1016.95 3.05444 1019.33 5.99996 1019.33C8.94547 1019.33 11.3333 1016.95 11.3333 1014C11.3333 1011.05 8.94547 1008.67 5.99996 1008.67C3.05444 1008.67 0.666623 1011.05 0.666623 1014ZM5 6L4.99996 1014L6.99996 1014L7 6L5 6Z" fill="#BB1605" />
+                            </svg>
                         </div>
 
-                        <div className="w-[859px] max-lg:w-[340px] max-lg:h-[176px] h-[184px] border-[1px] border-[#BB1605]">
-                            <div className="flex flex-row max-sm:flex-col max-sm:space-y-5 lg:space-x-20 py-10 max-sm:py-5 lg:items-center h-full max-sm:justify-center max-sm:pl-[3rem]  lg:pl-10">
-                                <p className="text-[#BB1605] max-sm:font-medium max-sm:text-[20px] font-bold text-[28px]">PHASE 3</p>
-
-                                <ul>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">10,000 holders</li>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">MULTICHAIN BRIDGINF</li>
-                                    <li className="text-[#BB1605] max-sm:text-[14px] text-[20px]">TOPTIER PARTNERSHIPS</li>
-                                </ul>
-
-                            </div>
-                        </div>
 
                     </div>
 
                 </div>
-                <div className="absolute max-md:hidden 3xl:top-[65rem] top-[50rem] max-lg:top-[55rem] xl:top-[48rem] 2xl:top-[49rem] w-full">
+                <div className="absolute max-md:hidden 3xl:top-[65rem] top-[72rem] max-lg:top-[55rem] xl:top-[48rem] 2xl:top-[49rem] w-full">
                     <img
                         src="./assets/roadmapBottom.svg"
                         className="  w-full"
